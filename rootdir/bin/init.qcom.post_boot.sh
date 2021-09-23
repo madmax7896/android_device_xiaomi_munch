98b40ce189115ce5f69c22843622b37a02f83bc7
Cosmin Tanislav <demonsingur@gmail.com>
2021-09-24 02:44:26 +0300

    sm8250-common: Do not touch sleep disabled

--- rootdir/bin/init.qcom.post_boot.sh
+++ rootdir/bin/init.qcom.post_boot.sh
@@ -5867,7 +5867,6 @@ case "$target" in
         # device/target specific folder
         setprop vendor.dcvs.prop 0
 	setprop vendor.dcvs.prop 1
-    echo N > /sys/module/lpm_levels/parameters/sleep_disabled
     configure_memory_parameters
     ;;
 esac

ea4cc5ac58a6e85d19a4ee2a0f14d3bfd07491e7
madmax7896 <madmax78968@gmail.com>
2022-08-26 15:53:52 +0530

    munch: Update defconfig for IMMENSITY-X kernel

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -15,7 +15,7 @@ BUILD_BROKEN_DUP_RULES := true
 TARGET_SCREEN_DENSITY := 440
 
 # Kernel
-TARGET_KERNEL_CONFIG := vendor/munch_defconfig
+TARGET_KERNEL_CONFIG := munch_defconfig
 
 # Init
 TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):init_xiaomi_munch

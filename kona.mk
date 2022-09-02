39822c64d5eea7b7b30ae5fb8f17234e6d8d7bce
kubersharma001 <kubersharma001@gmail.com>
2022-09-02 14:54:46 +0530

    sm8250-common: Inherit developer gsi keys
    error:  build/make/target/product/gsi_keys.mk does not exist..
    
    Change-Id: I7ac37f3e104f1bc6dce504ca29fcbf078127b399

--- kona.mk
+++ kona.mk
@@ -7,8 +7,8 @@
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
-# Include GSI keys
-$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
 
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)

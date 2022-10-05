be25bf8cab56a8795fbd16b26fd1d0cfe9afa626
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-10-05 07:37:49 +0000

    munch: Patch vendor.qti.hardware.camera.postproc@1.0-service-impl.so
    The modified library NOPs out the instruction to change binder
    threadpool, allowing the library to load and camera to work on S.
    
    Change-Id: I1ef0ed29e4a2eb820826e5d7df1fa53c9e3df584
    Signed-off-by: madmax7896 <madmax78968@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -12,6 +12,9 @@ function blob_fixup() {
         vendor/lib64/camera/components/com.mi.node.watermark.so )
             $PATCHELF --add-needed "libpiex_shim.so" "${2}"
             ;;
+        vendor/lib64/vendor.qti.hardware.camera.postproc@1.0-service-impl.so)
+            $SIGSCAN -p "9A 0A 00 94" -P "1F 20 03 D5" -f "${2}"
+            ;;
     esac
 }
 
